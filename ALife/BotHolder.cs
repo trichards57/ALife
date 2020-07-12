19805d582601c0d13cbae3298e6e2c88b2327416
Tony Richards <trichards57@gmail.com>
2020-07-12 12:03:54 +0100

    Split BotHolder into a seperate file.

---
+++ ALife/BotHolder.cs
@@ -0,0 +1,53 @@
+﻿using ALife.Model;
+using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ALife
+{
+    public class BotHolder
+    {
+        private readonly Ellipse ellipse;
+        private readonly Line line;
+
+        public BotHolder(Canvas canvas, Bot bot)
+        {
+            Bot = bot;
+            ellipse = new Ellipse
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 1,
+            };
+            line = new Line
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+
+            canvas.Children.Add(ellipse);
+            canvas.Children.Add(line);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Bot Bot { get; }
+
+        public SolidColorBrush Color => new SolidColorBrush(System.Windows.Media.Color.FromArgb(Bot.Color.A, Bot.Color.R, Bot.Color.G, Bot.Color.B));
+
+        public void Update()
+        {
+            ellipse.Fill = Color;
+            ellipse.Width = Bot.Radius * 2;
+            ellipse.Height = Bot.Radius * 2;
+            Canvas.SetLeft(ellipse, Bot.Position.X - Bot.Radius);
+            Canvas.SetTop(ellipse, Bot.Position.Y - Bot.Radius);
+
+            line.X1 = Bot.Position.X;
+            line.Y1 = Bot.Position.Y;
+            line.X2 = Bot.Position.X + Bot.Radius * Math.Cos(Bot.Orientation);
+            line.Y2 = Bot.Position.Y + Bot.Radius * Math.Sin(Bot.Orientation);
+        }
+    }
+}
--- ALife/Renderer.cs
+++ ALife/Renderer.cs
@@ -3,51 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Shapes;
 
 namespace ALife
 {
-    public class BotHolder
-    {
-        private readonly Ellipse ellipse;
-        private readonly Line line;
-
-        public BotHolder(Canvas canvas, Bot bot)
-        {
-            Bot = bot;
-            ellipse = new Ellipse
-            {
-                Stroke = Brushes.Black,
-                StrokeThickness = 1,
-            };
-            line = new Line
-            {
-                Stroke = Brushes.Black,
-                StrokeThickness = 2
-            };
-
-            canvas.Children.Add(ellipse);
-            canvas.Children.Add(line);
-        }
-
-        public Bot Bot { get; }
-
-        public void Update()
-        {
-            ellipse.Fill = new SolidColorBrush(Color.FromArgb(Bot.Color.A, Bot.Color.R, Bot.Color.G, Bot.Color.B));
-            ellipse.Width = Bot.Radius * 2;
-            ellipse.Height = Bot.Radius * 2;
-            Canvas.SetLeft(ellipse, Bot.Position.X - Bot.Radius);
-            Canvas.SetTop(ellipse, Bot.Position.Y - Bot.Radius);
-
-            line.X1 = Bot.Position.X;
-            line.Y1 = Bot.Position.Y;
-            line.X2 = Bot.Position.X + Bot.Radius * Math.Cos(Bot.Orientation);
-            line.Y2 = Bot.Position.Y + Bot.Radius * Math.Sin(Bot.Orientation);
-        }
-    }
-
     public class Renderer
     {
         private readonly List<BotHolder> botHolders = new List<BotHolder>();
