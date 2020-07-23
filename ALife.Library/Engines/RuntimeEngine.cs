586c76ec114e07531010aba62c0cd589b51fc717
Tony Richards <trichards57@gmail.com>
2020-07-23 08:00:49 +0100

    Minor optimisation of the runtime engine.

--- ALife.Library/Engines/RuntimeEngine.cs
+++ ALife.Library/Engines/RuntimeEngine.cs
@@ -7,11 +7,11 @@ using System.Numerics;
 
 namespace ALife.Engines
 {
-    public class RuntimeEngine
+    public static class RuntimeEngine
     {
         private const int VisionLimitSquared = Bot.VisionLimit * Bot.VisionLimit;
 
-        public void UpdateBot(Bot bot)
+        public static void UpdateBot(Bot bot)
         {
             bot.Orientation = Helpers.NormaliseAngle(bot.Orientation + Helpers.IntToAngle(bot.Memory[(int)MemoryAddresses.TurnRight] - bot.Memory[(int)MemoryAddresses.TurnLeft]));
 
@@ -25,7 +25,7 @@ namespace ALife.Engines
             bot.Force += Vector2.Transform(netForce, rotation);
         }
 
-        public void UpdateMemory(Bot bot)
+        public static void UpdateMemory(Bot bot)
         {
             // Clear the instruction memory
             bot.SetMemory(MemoryAddresses.MoveUp, 0);
@@ -36,14 +36,33 @@ namespace ALife.Engines
             bot.SetMemory(MemoryAddresses.TurnRight, 0);
 
             // Populate the state memory
-            var rotation = Matrix4x4.CreateRotationZ(-bot.Orientation);
-            var speed = Vector2.Transform(bot.Speed, rotation);
+            bot.SetMemory(MemoryAddresses.Speed, bot.RelativeSpeed.Length());
+            bot.SetMemory(MemoryAddresses.SpeedForward, bot.RelativeSpeed.X);
+            bot.SetMemory(MemoryAddresses.SpeedRight, bot.RelativeSpeed.Y);
+            for (var i = 0; i < Bot.EyeCount; i++)
+                bot.SetMemory(MemoryAddresses.EyeFirst, i, bot.EyeDistances[i]);
+            bot.SetMemory(MemoryAddresses.FocusBotDistance, bot.EyeDistances[Bot.EyeCount / 2]);
+            bot.SetMemory(MemoryAddresses.MyEyeRefCount, bot.EyeRefCount);
 
-            bot.SetMemory(MemoryAddresses.SpeedForward, speed.X);
-            bot.SetMemory(MemoryAddresses.SpeedRight, speed.Y);
+            if (bot.FocussedBot != null)
+            {
+                bot.SetMemory(MemoryAddresses.FocusBotDistance, bot.EyeDistances[bot.FocusEye]);
+                bot.SetMemory(MemoryAddresses.FocusBotSpeed, bot.FocussedBotRelativeSpeed.Length());
+                bot.SetMemory(MemoryAddresses.FocusBotSpeedForward, bot.FocussedBotRelativeSpeed.X);
+                bot.SetMemory(MemoryAddresses.FocusBotSpeedRight, bot.FocussedBotRelativeSpeed.Y);
+                bot.SetMemory(MemoryAddresses.FocusBotEyeRefCount, bot.FocussedBot.EyeRefCount);
+            }
+            else
+            {
+                bot.SetMemory(MemoryAddresses.FocusBotDistance, 0);
+                bot.SetMemory(MemoryAddresses.FocusBotSpeed, 0);
+                bot.SetMemory(MemoryAddresses.FocusBotSpeedForward, 0);
+                bot.SetMemory(MemoryAddresses.FocusBotSpeedRight, 0);
+                bot.SetMemory(MemoryAddresses.FocusBotEyeRefCount, 0);
+            }
         }
 
-        public void UpdateVision(Bot bot, IEnumerable<Bot> allBots)
+        public static void UpdateVision(Bot bot, IEnumerable<Bot> allBots)
         {
             foreach (var e in bot.Eyes)
                 e.Clear();
@@ -86,6 +105,12 @@ namespace ALife.Engines
             }
 
             bot.EyeDistances = bot.Eyes.Select(e => e.Any() ? Bot.VisionLimit - e.Min(n => n.Distance) : 0).ToArray();
+
+            var middleEye = bot.Eyes[bot.FocusEye];
+            if (middleEye.Any())
+                bot.FocussedBot = middleEye.OrderBy(e => e.Distance).First().OtherBot;
+            else
+                bot.FocussedBot = null;
         }
     }
 }
